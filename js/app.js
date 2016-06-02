5b32fb94a7b049643e6721659ecf006205e0688b
Jordan Tompkins <jtompkins28@gmail.com>
2016-06-02 14:48:41 -0400

    progress!

---
+++ js/app.js
@@ -0,0 +1,32 @@
+$(document).foundation()
+
+var megaRoster = {
+  init: function() {
+    this.setupEventListeners();
+    this.count = 0;
+  },
+
+  setupEventListeners: function() {
+    document.querySelector('form#studentForm').onsubmit =
+    this.addStudent.bind(this);
+  },
+
+
+  addStudent: function(ev) {
+    ev.preventDefault();
+    var f = ev.currentTarget;
+    var studentName = f.studentName.value;
+    var listItem = this.buildListItem(studentName);
+    var studentList = document.querySelector('#studentList');
+    studentList.appendChild(listItem);
+    f.reset();
+    this.count += 1;
+  },
+
+  buildListItem: function(studentName) {
+    var listItem = document.createElement('li');
+    listItem.innerText = studentName;
+    return listItem;
+  },
+};
+megaRoster.init();
