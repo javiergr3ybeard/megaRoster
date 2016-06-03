6a9813da40cfd0d9f2556a7895d6fa8be9437159
Jordan Tompkins <jtompkins28@gmail.com>
2016-06-03 15:33:48 -0400

    I was actually on the right track the whole time

--- js/app.js
+++ js/app.js
@@ -17,7 +17,6 @@ var megaRoster = {
     var studentName = f.studentName.value;
     var listItem = this.buildListItem(studentName);
 
-    // studentList.appendChild(listItem);
     this.prependChild(this.studentList, listItem);
 
     f.reset();
@@ -32,7 +31,10 @@ var megaRoster = {
 
   buildListItem: function(studentName) {
     var listItem = document.createElement('li');
-    listItem.innerText = studentName;
+    var span = document.createElement('span');
+    span.innerText = studentName;
+    span.className = 'studentName';
+    listItem.appendChild(span);
     this.appendLinks(listItem);
 
     return listItem;
@@ -50,11 +52,35 @@ var megaRoster = {
     var promoteLink = this.buildLink({
       text: 'promote',
       handler: function() {
-        listItem.style.border = '2px CornflowerBlue dashed';
-      }
+        this.promote(listItem);
+      }.bind(this)
     });
+    var moveUpLink = this.buildLink({
+      text: 'Up',
+      className:'moveUp',
+      handler: function() {
+        this.moveUp(listItem);
+      }.bind(this)
+    });
+    var moveDownLink = this.buildLink({
+      text: 'down',
+      className: 'moveDown',
+      handler: function() {
+        this.moveDown(listItem);
+      }.bind(this)
+    });
+    span.appendChild(this.buildLink({
+          text: 'edit',
+          className: 'edit',
+          handler: function() {
+            this.toggleEditable(listItem.querySelector('span.studentName'));
+          }.bind(this)
+        }));
+
     listItem.appendChild(removeLink);
     listItem.appendChild(promoteLink);
+    listItem.appendChild(moveUpLink);
+    listItem.appendChild(moveDownLink)
     listItem.appendChild(span);
   },
 
@@ -63,7 +89,36 @@ var megaRoster = {
     link.href = "#";
     link.innerText = options.text;
     link.onclick = options.handler;
+    link.className += (options.className || '');
     return link;
+  },
+
+  toggleEditable: function(el) {
+    var toggleElement = el.parentElement.querySelector('a.edit');
+    if (el.contentEditable === 'true') {
+      el.contentEditable = 'false';
+      toggleElement.innerHTML = 'edit';
+    }
+    else {
+      el.contentEditable = 'true';
+      el.focus();
+      toggleElement.innerHTML = 'save';
+    }
+  },
+
+  promote: function(listItem) {
+    this.prependChild(this.studentList, listItem);
+  },
+  moveUp: function(listItem) {
+     if (listItem !== this.studentList.firstElementChild) {
+       var previousItem = listItem.previousElementSibling;
+       this.studentList.insertBefore(listItem, previousItem);
+     }
+   },
+  moveDown: function(listItem) {
+    if(listItem !== this.studentList.firstchild) {
+      this.moveUp(listItem.nextElementSibling);
+    }
   },
 };
 megaRoster.init('#studentList');
