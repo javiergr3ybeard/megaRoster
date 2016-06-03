6e94299b09f9218a372808c5963da19bae8a234f
Jordan Tompkins <jtompkins28@gmail.com>
2016-06-02 20:44:27 -0400

    created all buttons have not given them their appropriate functions

--- js/app.js
+++ js/app.js
@@ -19,16 +19,24 @@ var megaRoster = {
     var listItem = this.buildListItem(studentName);
     var studentList = document.querySelector('#studentList');
     studentList.appendChild(listItem);
+
+    this.prependChild(studentList, listItem);
+
     f.reset();
     this.count += 1;
+
     f.studentName.focus();
+  },
+
+  prependChild: function(parent, child) {
+    parent.insertBefore(child, parent.firstChild)
   },
 
   buildListItem: function(studentName) {
     var listItem = document.createElement('li');
     var removeLink = this.buildLink({
       text: 'remove',
-      handler: function() {
+      handler: function(){
         listItem.remove();
       }
     });
@@ -36,12 +44,34 @@ var megaRoster = {
       text: 'promote',
       handler: function(){
         listItem.style.border = '2px purple dashed';
+        studentList.appendChild(listItem);
       }
     });
+    var upLink = this.buildLink({
+      text:'up',
+      handler: function(){
+        listItem.style.border = '2px blue dashed';
+      }
+    });
+    var downLink = this.buildLink({
+      text:'down',
+      handler: function(){
+        listItem.style.border = '2px green dashed';
+      }
+    });
+    var editLink = this.buildLink({
+      text:'edit',
+      handler: function(){
+        listItem.style.border = '2px red dashed';
+      }
+    });
+
     listItem.innerText = studentName;
     listItem.appendChild(removeLink);
     listItem.appendChild(promoteLink);
-
+    listItem.appendChild(upLink);
+    listItem.appendChild(downLink);
+    listItem.appendChild(editLink);
     return listItem;
   },
 
